a0e2ab5f0e2364a2ba22f2c8d737bb5d5042eccf
Patrick <pa@arthurfamily.ws>
2014-07-29 17:52:29 -0400

    Changing it up a bit

--- demo.py
+++ demo.py
@@ -1,6 +1,11 @@
 name = raw_input("What is your name? ")
 
-if name == "Patrick":
-  print("{} Is my name, and I like python".format(name))
-else:
+while name != "Patrick":
   print("{} Nice to meet you, how do you like python".format(name, name))
+  print "Your name length: {0}".format(len(name))
+  raw_input("Press <ENTER> to exit\n")
+  name = raw_input("What is your name? ")
+else:
+  print("{} Is my name, and I like python".format(name))
+  print "Your name length: {0}".format(len(name))
+
